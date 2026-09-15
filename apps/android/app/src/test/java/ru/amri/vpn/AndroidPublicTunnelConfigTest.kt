0173d20b43390afd2ed765b96a58da8624339485
Treninem <trenin.98@gmail.com>
2026-09-15 14:39:04 +0300

    Update Android public tunnel MTU tests

--- apps/android/app/src/test/java/ru/amri/vpn/AndroidPublicTunnelConfigTest.kt
+++ apps/android/app/src/test/java/ru/amri/vpn/AndroidPublicTunnelConfigTest.kt
@@ -7,28 +7,28 @@ import org.junit.Test
 class AndroidPublicTunnelConfigTest {
     @Test
     fun acceptsLoopbackTransportPortAndIpv6SafeMtu() {
-        val config = AndroidPublicTunnelConfig(localSocksPort = 20800, mtu = 1420)
+        val config = AndroidPublicTunnelConfig(localSocksPort = 20800, safeInitialMtu = 1420)
         assertEquals(20800, config.localSocksPort)
-        assertEquals(1420, config.mtu)
+        assertEquals(1420, config.safeInitialMtu)
     }
 
     @Test
     fun rejectsInvalidPort() {
         assertThrows(IllegalArgumentException::class.java) {
-            AndroidPublicTunnelConfig(localSocksPort = 0, mtu = 1420)
+            AndroidPublicTunnelConfig(localSocksPort = 0, safeInitialMtu = 1420)
         }
         assertThrows(IllegalArgumentException::class.java) {
-            AndroidPublicTunnelConfig(localSocksPort = 65_536, mtu = 1420)
+            AndroidPublicTunnelConfig(localSocksPort = 65_536, safeInitialMtu = 1420)
         }
     }
 
     @Test
     fun rejectsMtuOutsideSharedControllerBounds() {
         assertThrows(IllegalArgumentException::class.java) {
-            AndroidPublicTunnelConfig(localSocksPort = 20800, mtu = 1279)
+            AndroidPublicTunnelConfig(localSocksPort = 20800, safeInitialMtu = 1279)
         }
         assertThrows(IllegalArgumentException::class.java) {
-            AndroidPublicTunnelConfig(localSocksPort = 20800, mtu = 1501)
+            AndroidPublicTunnelConfig(localSocksPort = 20800, safeInitialMtu = 1501)
         }
     }
 }
