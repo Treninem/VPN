6b59b02fa508ba099b60890ef8878402c2a92403
Treninem <trenin.98@gmail.com>
2026-09-15 07:44:31 +0300

    Generate Android UI resources from canonical AMRI SVG assets

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -2,12 +2,27 @@ plugins {
     id("com.android.application")
 }
 
-val amriIconSource = rootProject.layout.projectDirectory.file("../../assets/brand/amri-icon.png")
+val amriBrandDir = rootProject.layout.projectDirectory.dir("../../assets/brand")
+val amriIconSource = amriBrandDir.file("amri-icon.png")
 val generatedAmriIconRes = layout.buildDirectory.dir("generated/amri-icon-res")
 val generateAmriIconResource by tasks.registering(org.gradle.api.tasks.Copy::class) {
     from(amriIconSource)
     into(generatedAmriIconRes.map { it.dir("drawable-nodpi") })
     rename { "amri_app_icon.png" }
+}
+
+val generatedAmriUiRes = layout.buildDirectory.dir("generated/amri-ui-res")
+val generateAmriUiResources by tasks.registering(org.gradle.api.tasks.Copy::class) {
+    into(generatedAmriUiRes.map { it.dir("raw") })
+    from(amriBrandDir.file("background-mobile.svg")) {
+        rename { "amri_background_mobile.svg" }
+    }
+    from(amriBrandDir.file("vpn-power-on.svg")) {
+        rename { "amri_vpn_power_on.svg" }
+    }
+    from(amriBrandDir.file("vpn-power-off.svg")) {
+        rename { "amri_vpn_power_off.svg" }
+    }
 }
 
 android {
@@ -29,11 +44,14 @@ android {
 }
 
 android.sourceSets["main"].res.srcDir(generatedAmriIconRes.get().asFile)
+android.sourceSets["main"].res.srcDir(generatedAmriUiRes.get().asFile)
 
 tasks.named("preBuild").configure {
     dependsOn(generateAmriIconResource)
+    dependsOn(generateAmriUiResources)
 }
 
 dependencies {
+    implementation("com.caverock:androidsvg-aar:1.4")
     testImplementation("junit:junit:4.13.2")
 }
