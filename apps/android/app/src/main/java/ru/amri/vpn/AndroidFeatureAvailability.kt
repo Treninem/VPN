075008cdb2e1e30c2ae0305dbdbd690c55fa44f6
Treninem <trenin.98@gmail.com>
2026-09-17 17:04:21 +0300

    Remove unused Android feature availability stub

--- apps/android/app/src/main/java/ru/amri/vpn/AndroidFeatureAvailability.kt
+++
@@ -1,9 +0,0 @@
-package ru.amri.vpn
-
-/** Runtime support matrix used by the UI so reserved controls cannot imply active protection. */
-object AndroidFeatureAvailability {
-    const val DNS_POLICY_TOGGLE = false
-    const val APP_KILL_SWITCH_TOGGLE = false
-    const val LOCAL_LEARNING = false
-    const val SHARED_LEARNING = false
-}
